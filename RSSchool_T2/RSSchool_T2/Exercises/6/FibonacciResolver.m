7b47c37195b4608a767adf916ad82a83edb692de
Pavel Germanovich <Germanovich>
2020-03-23 23:03:20 +0300

    feat: implement 6 task

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,32 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    NSInteger trueInt = 1;
+    NSInteger falseInt = 0;
+    NSMutableArray *arrayNs = [[NSMutableArray alloc] init];
+    int firstNumber = 0;
+    int secondNumber = 1;
+    
+    while (true) {
+        if (firstNumber * secondNumber < [number intValue]) {
+            NSInteger temp = secondNumber;
+            secondNumber += firstNumber;
+            firstNumber = temp;
+        }
+        else if (secondNumber * firstNumber == [number intValue]) {
+            [arrayNs addObject: [NSNumber numberWithInt:firstNumber]];
+            [arrayNs addObject: [NSNumber numberWithInt:secondNumber]];
+            [arrayNs addObject: [NSNumber numberWithInt:trueInt]];
+            break;
+        }
+        else {
+            [arrayNs addObject: [NSNumber numberWithInt:firstNumber]];
+            [arrayNs addObject: [NSNumber numberWithInt:secondNumber]];
+            [arrayNs addObject: [NSNumber numberWithInt:falseInt]];
+            break;
+        }
+    }
+    
+    return arrayNs;
 }
 @end
